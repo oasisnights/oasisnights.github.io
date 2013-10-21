e3c98cda77bab267c99445ed312aaa5858988a3b
Stephen Bussard <me@stephenbussard.com>
2013-10-21 15:20:04 -0600

    google analytics

--- js/script.js
+++ js/script.js
@@ -18,3 +18,12 @@ $(function() {
 	var str = 'info' + '\x40' + 'oasisnights' + '\x2e' + 'org';
 	$('.info').attr('href', 'mailto:' + str).html(str);
 });
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44985899-1', 'oasisnights.org');
+ga('send', 'pageview');
