dce5b5660b4062cfc4a3b932ef4375a457a7ef8b
Stephen Bussard <me@stephenbussard.com>
2013-10-21 15:49:55 -0600

    spec compliance

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 $(function() {
 	$.fn.parallax = function(st, r, offset) {
 		var offset = typeof offset !== 'undefined' ? offset : 0;
-		$(this).css('backgroundPositionY', (parseInt($(this).css('backgroundPositionY'), 10) - st) * r + offset);
+		var posY = $(this).css('backgroundPosition').split(' ')[1];
+		posY = posY.substring(0, posY.length - 2);
+		var newOffset = (posY - st) * r + offset;
+		$(this).css('backgroundPosition', "50% " + newOffset + "px");
 	}
 
 	$(window).scroll(function(event) {
 		var wh = $(this).height();
 		var st = $(document).scrollTop();
 		var $body = $('body');
-		var $shapes = $('[shapes]');
-		var $logo = $('[logo]');
+		var $shapes = $('#shapes');
+		var $logo = $('#logo');
 		$body.parallax(st, 0.3);
-		$shapes.parallax(st, -0.5);
+		$shapes.parallax(st, -0.5, 100);
 		$logo.parallax(st, -0.05, 100);
 	});
 
