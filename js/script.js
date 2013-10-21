b1cfd36c060699b7156aa34e4004d5c2be87511b
Stephen Bussard <me@stephenbussard.com>
2013-10-21 14:42:53 -0600

    email obfuscation and parallax adjustment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function() {
-	$.fn.parallax = function(st, r) {
-		$(this).css('backgroundPositionY', (parseInt($(this).css('backgroundPositionY'), 10) - st) * r);
+	$.fn.parallax = function(st, r, offset) {
+		var offset = typeof offset !== 'undefined' ? offset : 0;
+		$(this).css('backgroundPositionY', (parseInt($(this).css('backgroundPositionY'), 10) - st) * r + offset);
 	}
 
 	$(window).scroll(function(event) {
@@ -9,8 +10,11 @@ $(function() {
 		var $body = $('body');
 		var $shapes = $('[shapes]');
 		var $logo = $('[logo]');
-		$body.parallax(st, 0.1);
-		$shapes.parallax(st, 0.4);
-		$logo.parallax(st, 0.07);
+		$body.parallax(st, 0.3);
+		$shapes.parallax(st, -0.5);
+		$logo.parallax(st, -0.05, 100);
 	});
+
+	var str = 'info' + '\x40' + 'oasisnights' + '\x2e' + 'org';
+	$('.info').attr('href', 'mailto:' + str).html(str);
 });
